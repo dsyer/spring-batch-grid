452dfbf875e330e371125814ebf41362b7778b67
Dave Syer <dsyer@vmware.com>
2010-05-08 11:26:06 +0100

    tweaks of HSQL

--- spring-batch-gridgain/src/test/java/org/springframework/batch/core/partition/gridgain/ExampleJobConfigurationTests.java
+++ spring-batch-gridgain/src/test/java/org/springframework/batch/core/partition/gridgain/ExampleJobConfigurationTests.java
@@ -23,7 +23,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.launch.JobLauncher;
